dca39edb7797287a09e7932f391957bca76c419a
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-08 19:04:25 +0300

    Initial Commit: Implementing the Views, Partials and the controllers.

---
+++ views/partials/footer.php
@@ -0,0 +1,6 @@
+</div>
+
+<div class="flex flex-grow justify-center bg-white inset-shadow-sm inset-shadow-gray-200 fixed inset-x-0 bottom-0">Made with Love 💓 by PHP <?= date('Y') ?></div>
+</body>
+
+</html>
