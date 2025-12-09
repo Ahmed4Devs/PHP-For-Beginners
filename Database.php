88e88ad22a1e876fc53a5877f45d84e1595f5897
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-09 08:40:47 +0300

    Implement Database class and make a PDO connection to our mysql database, then make config file (same like env in js projects).

---
+++ Database.php
@@ -0,0 +1,50 @@
+<?php
+
+// Connect to the database, and execute a query.
+// this :: mean Scope Resolution Operator
+class Database
+{
+  public $connection;
+
+  public function __construct($config, $username = 'root', $password = 'toor')
+  {
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
+
+    $this->connection = new PDO($dsn, $username, $password, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+  }
+
+  public function query($query)
+  {
+    $statement = $this->connection->prepare($query);
+
+    $statement->execute();
+
+    return $statement;
+  }
+}
+
+
+
+/* Version 1 of Database Class
+class Database
+{
+  public $connection;
+
+  public function __construct()
+  {
+    $dsn = 'mysql:host=localhost;post=3306;dbname=myapp;user=root;password=toor;charset=utf8mb4';
+
+    $this->connection = new PDO($dsn);
+  }
+
+  public function query($query)
+  {
+    $statement = $this->connection->prepare($query);
+
+    $statement->execute();
+
+    return $statement;
+  }
+}*/
---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+  'database' => [
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'myapp',
+    'charset' => 'utf8mb4'
+  ],
+
+  // 'services' => [
+  //   'some provider' => [
+  //     'token' => '',
+  //     'secret_key' => '12334567890',
+  //   ]
+  // ]
+];
