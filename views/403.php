6422fba8ddebac752074c20d9b7b165a76df1fb6
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-10 07:51:00 +0300

    Implement simple authorization for view signle note for the user

--- views/403.php
+++ views/403.php
@@ -3,9 +3,9 @@
 
 <main class="flex justify-center h-screen items-center">
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
+    <h1 class="text-2xl font-bold">You are not authorized to view this page.</h1>
     <p class="mt-4">
-      ⬅️ <a href="/" class="text-blue hover:text-blue-500 hover:underline">Go Back Home</a>
+      ⬅️ <a href="/" class="text-blue underline hover:text-blue-500">Go Back Home</a>
     </p>
   </div>
 </main>
