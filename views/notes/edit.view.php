a2e6b32a92ac3bd37da751e72a85b495ae82e74e
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-12 10:55:34 +0300

    Implement updating with patch request

---
+++ views/notes/edit.view.php
@@ -0,0 +1,41 @@
+<?php require basePath('views/partials/head.php') ?>
+<?php require basePath('views/partials/nav.php') ?>
+<?php require basePath('views/partials/banner.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+
+    <form method="POST" action="/note">
+      <input type="hidden" name="_method" value="PATCH">
+      <input type="hidden" name="id" value="<?= $note['id'] ?>">
+
+      <div class="col-span-full">
+        <label for="body" class="block text-sm/6 font-medium text-gray-900 ml-[5px]">Body</label>
+        <div class="mt-2">
+          <textarea
+            id="body"
+            name="body"
+            rows="3"
+            class="block w-[75%] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            placeholder="Here's an idea for a note."><?= $note['body'] ?></textarea>
+          <?php if (isset($errors['body'])) : ?>
+            <p class="text-red-500 text-sm text-semibold"><?= $errors['body'] ?></p>
+          <?php endif; ?>
+        </div>
+      </div>
+
+      <div class="mt-6 flex items-center justify-start gap-x-6">
+        <a
+          href="/notes"
+          class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Cancel
+        </a>
+        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+      </div>
+    </form>
+
+  </div>
+</main>
+
+
+<?php require basePath('views/partials/footer.php') ?>
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,11 +10,10 @@
 
     <p><?= $note['body'] ?></p>
 
-    <form method="POST" class="mt-4">
-      <input type="hidden" name="_method" value="DELETE">
-      <input type="hidden" name="id" value="<?= $note['id'] ?>">
-      <button class="text-sm text-red-500">Delete</button>
-    </form>
+
+    <footer class="mt-6">
+      <a href="/note/edit?id=<?= $note['id'] ?>" class="rounded-md bg-gray-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Edit</a>
+    </footer>
   </div>
 </main>
 
