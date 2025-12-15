36e1b3776efd579d1bf47f0c877291460d9d22f7
Ahmed Fulayfil <ahmed4devs@gmail.com>
2025-12-15 18:58:31 +0300

    Flash old form  data to the session

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -13,7 +13,9 @@
         <div>
           <label for="email" class="block text-sm/6 font-medium text-gray-900">Email Address</label>
           <div class="mt-2">
-            <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              placeholder="Email address"
+              value="<?= old('email') ?>" />
           </div>
         </div>
 
@@ -22,7 +24,7 @@
             <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+            <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Password" />
           </div>
         </div>
 
